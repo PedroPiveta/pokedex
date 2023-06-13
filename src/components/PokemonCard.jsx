9396dbd52e2c0512b7223b169b90cf0eb549d385
Pedro Piveta Barrotti <ppivetabarrotti208@gmail.com>
2023-06-13 19:03:12 -0300

    fixed everything

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import useFetch from "../hooks/useFetch";
-import InfiniteScroll from 'react-infinite-scroll-component';
 import { css } from "@stitches/react";
 
 const pokemonCards = css({
@@ -32,6 +31,24 @@ const PokemonCard = () => {
     const { data: pokemon, loading, error } = useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`);
 
     useEffect(() => {
+        function handleScroll() {
+            const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+            const isAtBottom = scrollTop + clientHeight >= scrollHeight;
+
+            if (isAtBottom && !loading && !error && limit < pokemon.count) {
+                setLimit(prevLimit => prevLimit + 12);
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [loading, error, limit, pokemon]);
+
+
+    useEffect(() => {
         async function fetchPokemon() {
             try {
                 if (pokemon && pokemon.results) {
@@ -53,29 +70,23 @@ const PokemonCard = () => {
 
     return (
         <>
-            <InfiniteScroll
-                dataLength={pokemon.length} 
-                next={() => setLimit(prevLimit => prevLimit + 12)}
-                loader={<h4>Carregando...</h4>} 
-            >
-                <h1>Pokédex with PokéApi by <a href="https://github.com/PedroPiveta" target="_blank" rel="noreferrer" >Pedro Piveta</a></h1>
-                {loading && <p>Loading...</p>}
-                {error && <p>Error...</p>}
-                {pokemonList && (
-                    <div>
-                        {/* <button onClick={loadMore} >Load</button> */}
-                        <ul className={pokemonCards()}>
-                            {pokemonList.map((pokemon, index) => (
-                                <li className={pokemonCard()} key={index}>
-                                    {pokemon.name}
-                                    <img src={pokemon.sprites.front_default} alt="pokemon" />
-                                    <p>Heigth: {pokemon.height}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </InfiniteScroll>
+            <h1>Pokédex with PokéApi by <a href="https://github.com/PedroPiveta" target="_blank" rel="noreferrer" >Pedro Piveta</a></h1>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error...</p>}
+            {pokemonList && (
+                <div>
+                    {/* <button onClick={loadMore} >Load</button> */}
+                    <ul className={pokemonCards()}>
+                        {pokemonList.map((pokemon, index) => (
+                            <li className={pokemonCard()} key={index}>
+                                {pokemon.name}
+                                <img src={pokemon.sprites.front_default} alt="pokemon" />
+                                <p>Heigth: {pokemon.height}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </>
     );
 };
